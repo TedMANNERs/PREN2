66365afc4f137fa19d9d3ec0bf998b0e32e3e994
TedMANNERs <mannersofted@gmail.com>
2020-04-09 00:30:09 +0200

    Add pylons and horwbot, implement virtual LLC

---
+++ Simulation/Assets/Scripts/LowLevelController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal enum CommandType : byte
+    {
+        Start = 0x01,
+        SendTargetVector = 0x02,
+        SendSensorData = 0x03,
+        PlayAudio = 0x04,
+        Stop = 0x0,
+        Led = 0xF0
+    }
+
+    internal enum AudioCommand : byte
+    {
+        ShortBeep = 0x01,
+        LongBeep = 0x02
+    }
+
+    internal enum LEDCommand : byte
+    { 
+        Off = 0x00,
+        On = 0x01
+    }
+
+    public class LowLevelController : MonoBehaviour
+    {
+        private SerialPort _serialPort;
+        private Task _listenerTask;
+        private readonly CancellationTokenSource _listenerTokenSource = new CancellationTokenSource();
+        private CancellationToken _cancellationToken;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _serialPort = new SerialPort("COM4", 115200, Parity.Odd,8, StopBits.One);
+            _cancellationToken = _listenerTokenSource.Token;
+            _listenerTask = Task.Run(Listen, _cancellationToken);
+            _serialPort.WriteTimeout = 100;
+            _serialPort.Open();
+            byte[] startBuffer = {(byte) CommandType.Start};
+            _serialPort.Write(startBuffer, 0, startBuffer.Length);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnDestroy()
+        {
+            _listenerTokenSource.Cancel();
+            _serialPort.Close();
+        }
+
+        private void Listen()
+        {
+            while (!_cancellationToken.IsCancellationRequested)
+            {
+                if (_serialPort.IsOpen)
+                {
+                    try
+                    {
+                        int data = _serialPort.ReadByte();
+                        CommandType commandType = (CommandType)data;
+                        switch (commandType)
+                        {
+                            case CommandType.SendTargetVector:
+                                byte[] buffer = new byte[4]; // 4 = 2 x 2 signed int 16
+                                _serialPort.Read(buffer, 0, 4);
+
+                                if (BitConverter.IsLittleEndian)
+                                {
+                                    Array.Reverse(buffer, 0, 2);
+                                    Array.Reverse(buffer, 2, 2);
+                                }
+
+                                short speed = BitConverter.ToInt16(buffer, 0);
+                                short angle = BitConverter.ToInt16(buffer, 2);
+                                TargetVectorReceived(speed, angle);
+                                break;
+                            case CommandType.PlayAudio:
+                                AudioCommand audioCommand = (AudioCommand)_serialPort.ReadByte();
+                                Debug.Log($"Received AudioCommand = {audioCommand}");
+                                break;
+                            case CommandType.Stop:
+                                Debug.Log("Received Stop");
+                                break;
+                            case CommandType.Led:
+                                LEDCommand ledCommand = (LEDCommand)_serialPort.ReadByte();
+                                Debug.Log($"Received LEDCommand = {ledCommand}");
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception);
+                        Debug.Log(exception);
+                    }
+                }
+            }
+        }
+
+        private void TargetVectorReceived(short speed, short angle)
+        {
+            // TODO: Implement movement
+            Debug.Log($"Received TargetVector: Speed = {speed}, angle = {angle}");
+        }
+    }
+}
