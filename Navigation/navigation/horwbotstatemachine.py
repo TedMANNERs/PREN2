604c8598501590c63bd1b06ece97cdbde24ec44d
majkel_z <majkel_z@hotmail.com>
2020-04-28 02:02:37 +0200

    made a class in horwbotstatemachine.py

--- Navigation/navigation/horwbotstatemachine.py
+++ Navigation/navigation/horwbotstatemachine.py
@@ -1,26 +1,32 @@
 from transitions.extensions import HierarchicalMachine as Machine
 
-state = ['init', 'ready', 'error', {'name': 'running', 'children':['searching', 'movingToPylon', 'reversing', 'crossingObstacle', 'stopped', 'emergencyMode', 'parcourCompleted']}]
-transitions = [
-        ['initializing', 'init', 'ready'],
-        ['startSearching', 'ready', 'running_searching'],
-        ['moveForward', 'running_searching', 'running_movingToPylon'],
-        ['moveBackward', 'running_searching', 'running_reversing'],
-        ['crossing', 'running_searching', 'running_crossingObstacle'],
-        ['stopping', 'running_searching', 'running_stopped'],
-        ['goToEmergency', 'running_searching', 'running_emergencyMode'],
-        ['backToSearch', ['running_movingToPylon', 'running_reversing', 'running_crossingObstacle', 'running_stopped', 'running_emergencyMode'], 'running_searching'],
-        ['stayInSearch', 'running_searching', '='],
-        ['endParcour', 'running_searching', 'running_parcourCompleted'], 
-        ['errorHandling', 'error', 'ready'],
-        ['goToError', '*', 'error']
-]
+class HorwbotStateMachine(object):
+    state = ['init', 'ready', 'error', {'name': 'running', 'children':['searching', 'movingToPylon', 'reversing', 'crossingObstacle', 'stopped', 'emergencyMode', 'parcourCompleted']}]
+    transitions = [
+            ['initializing', 'init', 'ready'],
+            ['startSearching', 'ready', 'running_searching'],
+            ['moveForward', 'running_searching', 'running_movingToPylon'],
+            ['moveBackward', 'running_searching', 'running_reversing'],
+            ['crossing', 'running_searching', 'running_crossingObstacle'],
+            ['stopping', 'running_searching', 'running_stopped'],
+            ['goToEmergency', 'running_searching', 'running_emergencyMode'],
+            ['backToSearch', ['running_movingToPylon', 'running_reversing', 'running_crossingObstacle', 'running_stopped', 'running_emergencyMode'], 'running_searching'],
+            ['stayInSearch', 'running_searching', '='],
+            ['endParcour', 'running_searching', 'running_parcourCompleted'], 
+            ['errorHandling', 'error', 'ready'],
+            ['goToError', '*', 'error']
+    ]
 
-m = Machine(states=state, transitions=transitions, initial='init')
+    def __init__(self, name):
+        self.name = name
+        self.machine = Machine(model=self, states=HorwbotStateMachine.state, transitions=HorwbotStateMachine.transitions, initial='init')
+
+
 
 # Tests
 # TODO Extract to Testclass
 # TODO Extend tests
+m = HorwbotStateMachine("m")
 print(m.state)
 m.initializing()
 print(m.state)
