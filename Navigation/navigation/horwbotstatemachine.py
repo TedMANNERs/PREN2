517e483a7a87da6bd1855cc3bd2bd47b72c611ad
majkel_z <majkel_z@hotmail.com>
2020-04-29 00:35:35 +0200

    Implemented first transition: Not tested so far.

--- Navigation/navigation/horwbotstatemachine.py
+++ Navigation/navigation/horwbotstatemachine.py
@@ -1,9 +1,12 @@
 from transitions.extensions import HierarchicalMachine as Machine
+from navigation.initializing import Initializing
+
 
 class HorwbotStateMachine(object):
+    init_instance = None
     state = ['init', 'ready', 'error', {'name': 'running', 'children':['searching', 'movingToPylon', 'reversing', 'crossingObstacle', 'stopped', 'emergencyMode', 'parcourCompleted']}]
     transitions = [
-            { 'trigger': 'initializing', 'source': 'init', 'dest': 'ready', 'prepare': 'hello'},
+            { 'trigger': 'initializing', 'source': 'init', 'dest': 'ready', 'prepare': 'execute_initializing'},
             { 'trigger': 'startSearching', 'source': 'ready', 'dest': 'running_searching'},
             { 'trigger': 'moveForward', 'source': 'running_searching', 'dest': 'running_movingToPylon'},
             { 'trigger': 'moveBackward', 'source': 'running_searching', 'dest': 'running_reversing'},
@@ -20,9 +23,28 @@ class HorwbotStateMachine(object):
     def __init__(self):
         self.machine = Machine(model=self, states=HorwbotStateMachine.state, transitions=HorwbotStateMachine.transitions, initial='init')
 
-    def hello(self): print("Hello")
+    def execute_initializing(self): 
+        self.init_instance = Initializing()
+        self.init_instance.initializing_navigation_components()
+       
+    def execute_startSearching(self): pass
 
-m = HorwbotStateMachine()
-print(m.state)
-m.initializing()
+    def execute_moveForward(self): pass
+       
+    def execute_moveBackward(self): pass
+    
+    def execute_crossing(self): pass
+      
+    def execute_stopping(self): pass
+        
+    def execute_goToEmergency(self): pass
+    
+    def execute_backToSearch(self): pass
 
+    def execute_stayInSearch(self): pass
+
+    def execute_endParcour(self): pass
+
+    def execute_errorHandling(self): pass
+
+    def execute_goToError(self): pass
--- Navigation/navigation/initializing.py
+++ Navigation/navigation/initializing.py
@@ -1,1 +1,20 @@
-class Initializing()
+import signal
+import sys
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+from mission_control import MissionControl
+from communication.lowLevelController import LowLevelController, AudioCommand, LEDCommand
+from communication.usb import Usb
+from navigation.navigator import Navigator
+from imageDetection.pylonDetector import PylonDetector
+from debugGui.webserver import Webserver
+
+class Initializing():
+    
+    def __init__(self):
+        self.lowLevelController = LowLevelController()
+        self.missionControl = MissionControl(lowLevelController, Navigator(), PylonDetector())
+        #TODO signal.signal(signal.SIGINT, stop)
+        if Usb.hasWifiDongle():
+            self.webserver = Webserver(missionControl)
+            self.webserver.start()
