186d2fda65c02f84ddf482a68ec87633d8ed856f
majkel_z <majkel_z@hotmail.com>
2020-04-28 18:26:54 +0200

    changed transitions of statemachine to include callbacks

--- Navigation/navigation/horwbotstatemachine.py
+++ Navigation/navigation/horwbotstatemachine.py
@@ -3,22 +3,26 @@ from transitions.extensions import HierarchicalMachine as Machine
 class HorwbotStateMachine(object):
     state = ['init', 'ready', 'error', {'name': 'running', 'children':['searching', 'movingToPylon', 'reversing', 'crossingObstacle', 'stopped', 'emergencyMode', 'parcourCompleted']}]
     transitions = [
-            ['initializing', 'init', 'ready'],
-            ['startSearching', 'ready', 'running_searching'],
-            ['moveForward', 'running_searching', 'running_movingToPylon'],
-            ['moveBackward', 'running_searching', 'running_reversing'],
-            ['crossing', 'running_searching', 'running_crossingObstacle'],
-            ['stopping', 'running_searching', 'running_stopped'],
-            ['goToEmergency', 'running_searching', 'running_emergencyMode'],
-            ['backToSearch', ['running_movingToPylon', 'running_reversing', 'running_crossingObstacle', 'running_stopped', 'running_emergencyMode'], 'running_searching'],
-            ['stayInSearch', 'running_searching', '='],
-            ['endParcour', 'running_searching', 'running_parcourCompleted'], 
-            ['errorHandling', 'error', 'ready'],
-            ['goToError', '*', 'error']
+            { 'trigger': 'initializing', 'source': 'init', 'dest': 'ready', 'prepare': 'hello'},
+            { 'trigger': 'startSearching', 'source': 'ready', 'dest': 'running_searching'},
+            { 'trigger': 'moveForward', 'source': 'running_searching', 'dest': 'running_movingToPylon'},
+            { 'trigger': 'moveBackward', 'source': 'running_searching', 'dest': 'running_reversing'},
+            { 'trigger': 'crossing', 'source': 'running_searching', 'dest': 'running_crossingObstacle'},
+            { 'trigger': 'stopping', 'source': 'running_searching', 'dest': 'running_stopped'},
+            { 'trigger': 'goToEmergency', 'source': 'running_searching', 'dest': 'running_emergencyMode'},
+            { 'trigger': 'backToSearch', 'source': ['running_movingToPylon', 'running_reversing', 'running_crossingObstacle', 'running_stopped', 'running_emergencyMode'], 'dest': 'running_searching'},
+            { 'trigger': 'stayInSearch', 'source': 'running_searching', 'dest': '='},
+            { 'trigger': 'endParcour', 'source': 'running_searching', 'dest': 'running_parcourCompleted'},
+            { 'trigger': 'errorHandling', 'source': 'error', 'dest': 'ready'},
+            { 'trigger': 'goToError', 'source': '*', 'dest': 'error'}
     ]
 
     def __init__(self):
         self.machine = Machine(model=self, states=HorwbotStateMachine.state, transitions=HorwbotStateMachine.transitions, initial='init')
 
+    def hello(self): print("Hello")
 
+m = HorwbotStateMachine()
+print(m.state)
+m.initializing()
 
