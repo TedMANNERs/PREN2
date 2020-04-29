1dc0df4da056125af5677d781425e9e6489c3f76
TedMANNERs <mannersofted@gmail.com>
2020-04-29 19:21:05 +0200

    Update CAD model in simulation

---
+++ Simulation/Assets/WheelController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    private HingeJoint _joint;
+    [SerializeField]
+    private int _targetVelocity = 100;
+
+    [SerializeField]
+    private int _force = 100;
+
+    [SerializeField]
+    private KeyCode _key = KeyCode.UpArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _joint = GetComponent<HingeJoint>();
+        _joint.useMotor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(_key))
+        {
+            var jointMotor = _joint.motor;
+            jointMotor.targetVelocity = _targetVelocity;
+            jointMotor.force = _force;
+            _joint.motor = jointMotor;
+        }
+        else
+        {
+            _joint.motor = new JointMotor();
+        }
+    }
+}
