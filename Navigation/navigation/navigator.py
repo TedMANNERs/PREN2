fc01158b58962dd87629511eb51bd1ba1b2f3a66
TedMANNERs <mannersofted@gmail.com>
2020-05-07 00:52:10 +0200

    target standing pylons only

--- Navigation/navigation/navigator.py
+++ Navigation/navigation/navigator.py
@@ -17,10 +17,10 @@ class Navigator:
         [('obj_label', confidence, (bounding_box_x_px, bounding_box_y_px, bounding_box_width_px, bounding_box_height_px), distance)]
         The X and Y coordinates are from the center of the bounding box. Subtract half the width or height to get the lower corner.
         """
-        
-        if detectedPylons:
-            index = np.argmax([x[2][0] for x in detectedPylons])
-            rightmost_nearest_pylon = detectedPylons[index]
+        standingPylons = list(filter(lambda x: x[0].decode("utf-8") == "pylon", detectedPylons))
+        if standingPylons:
+            index = np.argmax([x[2][0] for x in standingPylons])
+            rightmost_nearest_pylon = standingPylons[index]
             print("Rightmost Nearest Pylon = {0}, Index = {1}".format(rightmost_nearest_pylon, index))
             frame_width = frame.shape[0]
             if rightmost_nearest_pylon[2][0] < (frame_width / 4): # check if the rightmost nearest pylon is in the left quarter of the frame
