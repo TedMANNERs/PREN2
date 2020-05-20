98b2e1e10575f179d8fb9f467e0e323c6e247f8e
TedMANNERs <mannersofted@gmail.com>
2020-05-20 18:58:21 +0200

    Add gstreamer pipeline for linux

--- Navigation/camera/camera_provider.py
+++ Navigation/camera/camera_provider.py
@@ -5,6 +5,7 @@ import logging
 from camera.camera import Camera
 from camera.camera_fake import CameraFake
 from camera.camera_simulation import CameraSimulation
+from configreader import parser
 
 class CameraProvider:
     _camera = None
@@ -14,7 +15,12 @@ class CameraProvider:
         if isSimulation:
             CameraProvider._camera = CameraSimulation()
         else:
-            cap = cv2.VideoCapture(0) # Use default camera (picamera on jetson, webcam on other devices)
+            cap = None
+            if os.name == "nt": #Windows
+                cap = cv2.VideoCapture(0) # Use default camera (picamera on jetson, webcam on other devices)
+            else:
+                GSTREAMER_PIPELINE = parser.get("gstreamer", "GSTREAMER_PIPELINE")
+                cap = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
             if cap is None or not cap.isOpened():
                 logging.warning("Warning: Default camera was not found or could not be opened. Continuing with static image...")
                 CameraProvider._camera = CameraFake()
