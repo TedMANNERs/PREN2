a63d3b51044e6eb2f69dd6181a0ff328ffdb6e57
TedMANNERs <mannersofted@gmail.com>
2020-05-20 22:35:21 +0200

    Fix null pointer access

--- Navigation/imageDetection/darknet.py
+++ Navigation/imageDetection/darknet.py
@@ -59,8 +59,7 @@ class DETECTION(Structure):
                 ("prob", POINTER(c_float)),
                 ("mask", POINTER(c_float)),
                 ("objectness", c_float),
-                ("sort_class", c_int),
-                ("uc", POINTER(c_float))]
+                ("sort_class", c_int)]
 
 
 class IMAGE(Structure):
