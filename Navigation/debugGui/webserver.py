0badae611fad7b99aa0c9f30d601c9a65e883a14
TedMANNERs <mannersofted@gmail.com>
2020-05-27 21:50:54 +0200

    Fix debug website

--- Navigation/debugGui/webserver.py
+++ Navigation/debugGui/webserver.py
@@ -13,7 +13,8 @@ class Webserver:
         self.app = Flask(__name__)
         self.app.add_url_rule("/", "index", self.index)
         self.app.add_url_rule("/video_feed", "video_feed", self.video_feed)
-        self.app.add_url_rule("/stream_logs", "stream_logs", self.video_feed)
+        self.app.add_url_rule("/stream_logs", "stream_logs", self.stream_logs)
+        self.app.add_url_rule("/state_diagram_feed", "state_diagram_feed", self.state_diagram_feed)
         self.__thread = threading.Thread(target=self._run, daemon=True)
         
     def index(self):
@@ -24,14 +25,18 @@ class Webserver:
         """Video streaming route. Put this in the src attribute of an img tag."""
         return Response(self._create_stream_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    def stream_logs(self):
-        def generate():
-            with open('../horwlog.log') as f:
-                while True:
-                    yield f.read()
-                    sleep(1)
+    def state_diagram_feed(self):
+        return Response(self._create_state_diagram_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-        return Response(stream_with_context(generate())) 
+    def stream_logs(self):
+        def _generate_logs():
+            with open('horwlog.log') as f:
+                while True:
+                    data= f.read()
+                    if not data:
+                        break
+                    yield data
+        return Response(stream_with_context(_generate_logs()), mimetype="text/plain") 
 
     def start(self):
         logging.info("Starting webserver")
@@ -40,11 +45,19 @@ class Webserver:
     def _run(self):
         self.app.run(host='0.0.0.0', port=8080, threaded=True)
 
+    # TODO: Implement update mechanism for all debug info -> update data from client (javscript with ajax)?
+    def _create_state_diagram_generator(self):
+        """Video streaming generator function."""
+        while True:
+            if DebugInfo.stateDiagram is None:
+                continue
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode(".jpg", DebugInfo.stateDiagram)[1].tobytes() + b'\r\n')
+
     def _create_stream_generator(self):
         """Video streaming generator function."""
         while True:
-            debugInfo = DebugInfo.getLatest() # TODO: Implement update mechanism for all debug info -> update data from client (javscript with ajax)?
-            if debugInfo.latestFrame is None:
+            if DebugInfo.latestFrame is None:
                 continue
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode(".jpg", debugInfo.latestFrame)[1].tobytes() + b'\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode(".jpg", DebugInfo.latestFrame)[1].tobytes() + b'\r\n')
